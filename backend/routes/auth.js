37864d45c3aade24693c812f4683fc7c30a34224
iujjwalrai <imujjwal.rai@gmail.com>
2026-01-14 14:58:39 +0530

    fixed cors issues

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -84,7 +84,7 @@ router.post('/login', async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: true,
-      sameSite: 'strict',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
